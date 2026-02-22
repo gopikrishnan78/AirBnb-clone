aa4aa264f50e8737e6d899ba9e5220d9508ee685
gopikrishnan78 <sgopikrishnan78@gmail.com>
2026-02-22 11:36:21 +0530

    Delete air bnb clone directory

--- air bnb clone/server/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadImages } = require('../controllers/uploadController');
-const auth = require('../middleware/auth');
-const upload = require('../middleware/upload');
-
-router.post('/images', auth, upload.array('images', 10), uploadImages);
-
-module.exports = router;
